e409731943b56ab3a6fc7304a1a0751af21c5078
Tehc <noahsturner06@gmail.com>
2019-08-18 16:46:54 -0400

    Add some commands and bug fixes
    Add some commands for later implementation and /spawn command bug fix

--- src/TEssentials/Main.php
+++ src/TEssentials/Main.php
@@ -7,21 +7,31 @@ namespace TEssentials;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
 use raklib\protocol\AdvertiseSystem;
 
 class Main extends PluginBase {
-	public function onEnable() : void{
-		$this->getLogger()->info('Loading...');
-		$this->getLogger()->info('Loaded!');
+	public function onEnable() : void{}
+
+	public function onPlayerLogin(Player $player) : void{
+		if (!$player->hasPlayedBefore()) {
+			//Set Default Spawn
+			$pos = new Vector3(0, 70, 0);
+			$player->setSpawn($pos);
+			unset($pos);
+			//Broadcast Welcome Message
+			$player->getServer()->broadcast('Welcome '+$player->getName()+' to '+$player->getServer()->getName()+'!');
+		}
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-		$plr = $sender->getServer()::getPlayerExact($sender->getName());
-
 		switch($command->getName()){
-			case "spawn":
-				$sender->sendMessage('Teleporting to spawn...');
-				$plr->setPosition($plr->getSpawn());
+			case 'spawn':
+				if ($sender->hasPermission('TEssentials.warp.spawn')){
+					$sender->sendMessage('Teleporting to spawn...');
+					$sender->getServer()->getPlayerExact($sender->getName())->setPosition($sender->getSpawn());
+				}
 				return true;
 			default:
 				return false;
